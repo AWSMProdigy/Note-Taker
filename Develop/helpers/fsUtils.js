e1217893b8c39b386014047c4b981977df13d67a
AWSMProdigy <StraightAKyle@gmail.com>
2021-10-21 21:00:22 -0400

    Full functionality achieved

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -31,4 +31,23 @@ const readAndAppend = (content, file) => {
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+const readAndDelete = (id, file) => {
+  fs.readFile(file, 'utf8', (err, data) =>{
+    if(err){
+      console.error(err);
+    }else{
+      const parsedData = JSON.parse(data);
+      for(var i=0; i < parsedData.length; i++){
+        console.log(parsedData[i].id);
+        if(parsedData[i].id == id){
+          console.log(`Note found: ${parsedData[i].title}`)
+          parsedData.splice(i, 1);
+        }
+      }
+      writeToFile(file, parsedData);
+    }
+  }
+  )
+}
+
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,11 +1,11 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 
 // GET Route for retrieving all the tips
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for tips`);
+  console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
@@ -14,13 +14,13 @@ notes.post('/', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
-  const { title, note} = req.body;
+  const { title, text} = req.body;
 
   if (req.body) {
     const newNote = {
+      id: uuid(),
       title,
-      note,
-      tip_id: uuid(),
+      text
     };
 
     readAndAppend(newNote, '../develop/db/db.json');
@@ -29,5 +29,13 @@ notes.post('/', (req, res) => {
     res.error('Error in adding note');
   }
 });
+
+notes.delete('/', (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
+      console.log("id: " + req.query.id);
+      readAndDelete(req.query.id, '../develop/db/db.json');
+      res.json(`Note deleted successfully 🚀`);
+
+  });
 
 module.exports = notes;
